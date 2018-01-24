28169fc0faa447d9dcdb834e7b20b43836521520
Egor Suvorov <egor.suvorov@gmail.com>
2018-01-25 02:44:35 +0300

    Start task3, looks like interleaving search is needed

---
+++ minikanren-impl/task3.hs
@@ -0,0 +1,70 @@
+module Task3 where
+import Minikanren
+import Lists
+
+fox = Func "fox" []
+goose = Func "goose" []
+beans = Func "beans" []
+empty = Func "empty" []
+
+left = Func "left" []
+right = Func "right" []
+
+goodSide :: Term -> Goal
+goodSide s = (notElemo fox s ||| notElemo goose s) &&& (notElemo goose s ||| notElemo beans s)
+
+state :: Term -> Term -> Term -> Term
+state l r boatPos = Func "state" [l, r, boatPos]
+
+goodState :: Term -> Goal
+goodState s = fresh $ \l -> fresh $ \r -> fresh $ \boatPos -> (state l r boatPos) === s &&& goodSide l &&& goodSide r
+
+finishState :: Term -> Goal
+finishState s = fresh $ \l -> fresh $ \r -> fresh $ \boatPos -> (state l r boatPos) === s &&& l === nil &&& boatPos === right
+
+canTransfer :: Term -> Term -> Term -> Term -> Term -> Goal
+canTransfer l1 r1 l2 r2 x =
+  (x === empty &&& l1 === l2 &&& r1 === r2) ||| (
+    fresh $ \h -> fresh $ \t -> l1 === h -:- t &&& (
+    (x === h &&& l2 === t &&& r2 === (h -:- r1)) ||| (
+      fresh $ \t' -> l2 === h -:- t' &&&
+        canTransfer t r1 t' r2 x
+    )
+    )
+  )
+
+canMove :: Term -> Term -> Term -> Goal
+canMove from to x =
+  fresh $ \l1 -> fresh $ \r1 -> fresh $ \s1 -> from === state l1 r1 s1 &&& (
+  fresh $ \l2 -> fresh $ \r2 -> fresh $ \s2 -> to === state l2 r2 s2 &&& (
+    (s1 === left &&& s2 === right &&& canTransfer l1 r1 l2 r2 x) |||
+    (s1 === right &&& s2 === left &&& canTransfer l2 r2 l1 r1 x)
+  )
+  )
+
+canMoveSeveral :: Term -> Term -> Term -> Goal
+canMoveSeveral from to xs =
+  (xs === nil &&& from === to) ||| (
+  fresh $ \h -> fresh $ \t -> xs === h -:- t &&& (
+    fresh $ \q -> canMove from q h &&& canMoveSeveral q to t
+  )
+  )
+
+showStr :: Term -> String
+showStr (Func x []) = x
+
+showStrs :: Term -> String
+showStrs s = show $ map showStr $ listToHlist s
+
+showState :: Term -> String
+showState (Func "state" [l, r, Func side []]) = show $ (showStrs l, showStrs r, side)
+
+showCommands :: Term -> String
+showCommands = showStrs
+
+initial :: Term
+initial = state (hlistToList [fox, goose, beans]) (hlistToList []) left
+
+main :: IO ()
+main = do
+    run showCommands 10 $ canMoveSeveral initial $ state (hlistToList [fox, goose, beans]) (hlistToList []) right
